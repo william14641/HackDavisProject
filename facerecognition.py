b6fc60c72868aa598a7d10654b3305ee2bbaf3dc
smarathe12 <marathesidd@gmail.com>
2021-01-16 12:05:27 -0800

    url is working now

--- facerecognition.py
+++ facerecognition.py
@@ -3,6 +3,7 @@ import json
 import cv2
 import numpy as np
 import urllib
+import urllib.request
 
 
 response = requests.get('https://api.fbi.gov/wanted/v1/list')
@@ -25,14 +26,22 @@ for item in wanted:
 		a=item['images'][0]['original']
 		urls.append(a)
 
+class AppURLopener(urllib.request.FancyURLopener):
+    version = "Mozilla/5.0"
+
+opener = AppURLopener()
+
 def url_to_image(url):
 	# download the image, convert it to a NumPy array, and then read
 	# it into OpenCV format
-	resp = urllib.request.urlopen(url)
+	resp = opener.open(url)
 	image = np.asarray(bytearray(resp.read()), dtype="uint8")
 	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 	# return the image
 	return image
+
+for url in urls:
+	url_to_image(url)
 
 
 
