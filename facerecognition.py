122508318398bc42319efcf26f07930e02b7610a
smarathe12 <marathesidd@gmail.com>
2021-01-16 11:49:02 -0800

    commit

--- facerecognition.py
+++ facerecognition.py
@@ -1,6 +1,8 @@
 import requests
 import json
-
+import cv2
+import numpy as np
+import urllib
 
 
 response = requests.get('https://api.fbi.gov/wanted/v1/list')
@@ -22,6 +24,17 @@ for item in wanted:
 	if item['subjects'] == ['Kidnappings and Missing Persons']:
 		a=item['images'][0]['original']
 		urls.append(a)
+
+def url_to_image(url):
+	# download the image, convert it to a NumPy array, and then read
+	# it into OpenCV format
+	resp = urllib.request.urlopen(url)
+	image = np.asarray(bytearray(resp.read()), dtype="uint8")
+	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+	# return the image
+	return image
+
+
 
         
 
