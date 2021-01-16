470f463d4691224ed36fb7b4ac655fd77c077b6c
William Xu <williamxu132@gmail.com>
2021-01-16 10:40:26 -0800

    image urls retrieved

---
+++ facerecognition.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+response = requests.get('https://api.fbi.gov/wanted/v1/list')
+data = json.loads(response.content)
+wanted=[]
+
+page = 1
+
+for i in range(1000):
+	response = requests.get('https://api.fbi.gov/wanted/v1/list?page=' + str(page))
+	data = json.loads(response.content)
+	if not data['items']:
+		break
+	wanted.extend(data['items'])
+	page = data['page'] + 1
+
+for item in wanted:
+	if item['subjects'] == ['Kidnappings and Missing Persons']:
+		print(item['images'][0]['original'])
+
+
